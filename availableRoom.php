fee2360bab06c9bb7d47a87e01c0cc166eac00f2
yukofksm <fukusuke.pipi@gmail.com>
2019-12-18 11:47:33 +0800

    updated dec 18

--- availableRoom.php
+++ availableRoom.php
@@ -1,9 +1,11 @@
 <?php
-    require_once 'class/book.php';
-    $book = new Book();
-    $booking = $book->checkDate($checkIn,$checkOut,$roomType,$adult,$kids);
+    require_once 'class/room.php';
+    $room = new Room();
+    $booking = checkDate($checkIn,$checkOut,$roomType);
 
-    print_r($booking);
+    echo "checking".$checkIn, $checkOut, $roomType;
+
+    // print_r($booking);
 
     session_start();
 
@@ -14,7 +16,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ALL ROOMS</title>
+    <title>AVAILABLE ROOMS</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/d98ab22c54.js" crossorigin="anonymous"></script>
 </head>
